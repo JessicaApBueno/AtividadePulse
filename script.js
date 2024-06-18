59d06cbc1071ad7d6063651ade1c3377df623d00
JessicaApBueno <buenojessicaaparecida@gmail.com>
2024-06-18 07:15:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    textoArray.forEach((letra, i) => {
+        elemento.innerHTML = '';
+        setTimeout(function(){
+            elemento.innerHTML += letra;
+        }, 75 * i)
+    });
+}
+
+const titulo = document.querySelector(".main-title");
+typeWriter(titulo);
